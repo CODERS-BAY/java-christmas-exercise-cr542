9d498bd30f7efee7f066c69642f9dddfff05c834
cr542 <christian.reisenauer@gmx.at>
2021-01-08 16:31:50 +0100

    Update Console.java

--- task1/view/Console.java
+++ task1/view/Console.java
@@ -3,8 +3,8 @@ package view;
 import java.util.Scanner;
 
 import controller.ElfController;
+import model.Child;
 import model.Present;
-import model.Sledge;
 
 public class Console {
 	
@@ -13,9 +13,9 @@ public class Console {
 	
 	public void dialog() {
 		System.out.println("Please login:");
-		String userName = userInput.nextLine();
+		String userName = userInput.next();
 		controller.setLogin(userName);
-		
+
 		displayMenu();
 	}
 	
@@ -32,71 +32,76 @@ public class Console {
 		System.out.println("[+] Hi Santa!");
 		System.out.println("[+] What do you want to do?");
 		System.out.println("[+] 1 = Display all Childs | 2 = Display all presents | 3 = Check sledge status");
+		System.out.println("[+] 4 = Search for a child | 5 = Get the sledge ready");
 		System.out.println("[+] Press any other key to logout.");
 		int choice = userInput.nextInt();
 		
 		switch (choice) {
 		case 1: 
 			controller.printAllChilds();
+			System.out.println();
 			displayMenu();
 		case 2: 
 			controller.showStorage();
+			System.out.println();
 			displayMenu();
 		case 3:
 			controller.printSledgeStatus();
+			System.out.println();
 			displayMenu();
+		case 4:
+			System.out.println();
+			searchDialog();
+		case 5:
+			controller.loadCargo();
+			controller.printSledgeStatus();
+			System.out.println("\n[System] Have a good flight, Santa! Logging you out now.\n");
 		default:
 			controller.logout();
+			System.out.println();
 			dialog();
 		}
 	}
 	
 	public void dialogChild() {
-		System.out.println("Adding a child for test purposes:\n");
-		
-		controller.addChild("Test", 12, "Teststadt");
-		
-		System.out.println();
-		
-		controller.printAllChilds();
-		dialog();
-	}
-	
-	public void dialogChildWIP() {
-		System.out.println("[*] Hi "+controller.getLoggedUser()+"! This is Santa Clause.");
+		String childName = controller.getLoggedUser();
+		System.out.println("[*] Hi "+childName+"! This is Santa Clause.");
 		System.out.println("[*] Please answer the following questions. This will make it easier for me to bring you a present!");
 		/*
 		 * Child specific questions
 		 */
-		String childName = controller.getLoggedUser();
-		System.out.println("[*] How old are you");
-		int childAge = userInput.nextInt();
+		System.out.println("[*] How old are you?");
+		int childAge = Integer.parseInt(userInput.next());
 		System.out.println("[*] Where do you live?");
-		String livingPlace = userInput.nextLine();
+		String livingPlace = userInput.next();
 		/*
 		 * Present specific questions
 		 */
 		System.out.println("[*] What do you want for Christmas?");
-		String presentName = userInput.nextLine();
+		String presentName = userInput.next();
 		System.out.println("[*] What's the weight of your present (just estimate)?");
-		double presentWeight = userInput.nextDouble();
+		double presentWeight = Double.parseDouble(userInput.next());
 		
-//		TODO Fix Scanner => Throws exception because of double/String/int
+		Child child = new Child(childName, childAge, livingPlace);
+		controller.addChild(child);
+		Present present = new Present(presentName, presentWeight, child);
+		controller.addPresent(present);
 		
-
-//		TODO Create object => Child
-		controller.addChild(childName, childAge, livingPlace);
-//		TODO Create object => Present
-//		controller.addPresent(presentName, presentWeight, ???);
-		
-		System.out.println("[*] Received your wish, "+childName);
+		System.out.println("\n[*] Received your wish, "+childName);
 		System.out.println("[*] Present: "+presentName+" | Weight: "+presentWeight+" | Age: "+childAge+" | Town: "+livingPlace);
 	
-		System.out.println("[*] Logging you out now! Thank you for using my brand new software!");
+		System.out.println("\n[*] Logging you out now! Thank you for using my brand new software!");
 		controller.logout();
 		dialog();
 	}
 	
+	public void searchDialog() {
+		System.out.println("[System] Which child should I search for?\n[System] Please enter the correct name below:");
+		String searchName = userInput.next();
+		
+		controller.search(searchName);
+		dialogSanta();
+	}
 	
 
 }
